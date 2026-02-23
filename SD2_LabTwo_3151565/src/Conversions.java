46950c837a8314076a1b7edf8bc1c6b2980e477a
Maksym Shtymak <maksym.shtymak@student.griffith.ie>
2026-02-23 19:47:34 +0200

    add comments

--- SD2_LabTwo_3151565/src/Conversions.java
+++ SD2_LabTwo_3151565/src/Conversions.java
@@ -1,6 +1,6 @@
 // Maksym Shtymak 3151565
 public class Conversions {
-
+    // basic conversions currency method with ternar check
     public double euroToDollar(double euro) {
         return euro > 0 ? euro * 1.1 : 0;
     }
@@ -9,18 +9,21 @@ public class Conversions {
         return dollar > 0 ? dollar * 0.9 : 0;
     }
 
+    // string to int conversion with null and empty check
     public int stringToInteger(String val) {
         return val != null && !val.isEmpty() ? Integer.parseInt(val) : 0;
     }
-
+    // int to string conversion
     public String integerToString(int val) {
         return String.valueOf(val);
     }
-
+    // switch case method
     public String switchCase(String val) {
+        // avoid null values
         if (val == null) {
             return null;
         }
+        // letter by letter case changing
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < val.length(); i++) {
             char c = val.charAt(i);
